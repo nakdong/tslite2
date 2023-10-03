b224e4b1952c998da35e953ba27c1c86c6d712d5
MakerZip <makerzip@hanmail.net>
2023-10-03 12:27:58 +0900

    Update tslite2.ino

--- tslite2/tslite2.ino
+++ tslite2/tslite2.ino
@@ -1,12 +1,15 @@
-const char* VERSION = "1.5";   // 코드 버전
+const char* VERSION = "1.9";   // 코드 버전
 
 #include <avr/wdt.h>
 #include <Adafruit_NeoPixel.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetoothSerial(A2, A3); // RX, TX pins
 
 #define LED_PIN    4         // 네오픽셀 데이터 핀
-#define LED_COUNT  30        // 네오픽셀의 개수
-#define YELLOW     0xFFAA00  // 노란색 (RGB 값)
-#define WHITE      0xFFFFFF  // 흰색 (RGB 값)
+#define LED_COUNT  34        // 네오픽셀의 개수
+#define YELLOW     0xFF5500  // 노란색 (RGB 값)
+#define WHITE      0xFFBFAF  // 흰색 (RGB 값)
 
 #define MIN_HANDLE 315       // 조향 각도 최소 값
 #define MAX_HANDLE 95        // 조향 각도 최대 값
@@ -21,7 +24,7 @@ const int motor2SpeedPin = 10;     // 모터 2 속도 제어 핀
 const int motor3DirectionPin = 12; // 모터 3 방향 핀
 const int motor3SpeedPin = 11;     // 모터 3 속도 제어 핀
 const int handlePin = A0;          // 가변저항 핀
-const int frontLedPin = 13;        // 전조등 핀, 13번 핀이라 작동될 때 ㅁ
+const int frontLedPin = 13;        // 전조등 핀
 const int backLedPin = 5;          // 후진등 핀
 const int handleRelay = 3;         // 헨들 전원 차단 핀
 const int batRelay = 2;            // 배터리 전원 차단 핀
@@ -54,6 +57,7 @@ void setup() {
     ; // 시리얼 포트가 준비될 때까지 대기합니다.
   }
   Serial.println("OK: booting");
+  bluetoothSerial.begin(9600); // 아두이노와 블루투스 모듈 간의 시리얼 통신
   // 핀 모드 설정
   pinMode(motor1DirectionPin, OUTPUT);
   pinMode(motor1SpeedPin, OUTPUT);
@@ -74,6 +78,8 @@ void setup() {
     strip.setPixelColor(i, 0); // 모든 픽셀 끄기
   }
   strip.show();
+  controlMotor(motor1DirectionPin, motor1SpeedPin, 0);
+  controlMotor(motor2DirectionPin, motor2SpeedPin, 0);
 }
 
 void loop() {
@@ -93,9 +99,9 @@ void loop() {
     } else if (inputString == "$reset\n") {
       softwareReset();
     } else if (inputString == "$stop\n") {
-      // "$start" 입력을 받을 때까지 대기합니다.
+      // "start" 입력을 받을 때까지 대기합니다.
       while (true) {
-        Serial.println("아두이노를 시작하려면 '$start'혹은 '$reset'을 입력하세요:");
+        Serial.println("아두이노를 시작하려면 '$start'를 입력하세요:");
         while (Serial.available() == 0) {
           // 입력이 없으면 대기합니다.
         }
@@ -115,6 +121,9 @@ void loop() {
       // parseCommand 함수 호출
       parseCommand(inputString);
     }
+
+    
+
     // inputString 초기화
     inputString = "";
     stringComplete = false;
@@ -164,7 +173,6 @@ void parseCommand(String command) {
   char batrelay[10];
 
   // 시리얼로 받은 명령을 분석
-  // #(왼쪽 바퀴 속도,double) (오른쪽 바퀴 속도,double) (조향 각도,double) (앞쪽 전등,bool) (뒤쪽 전등,bool) (깜빡이,left|right|all|off) (핸들 릴레이,bool) (배터리 릴레이,bool)
   int parsed = sscanf(command.c_str(), "#%d %d %d %s %s %s %s %s %s", &leftWheelSpeed, &rightWheelSpeed, &setpoint, frontled, backled, neopixel, handlerelay, batrelay);
 
   if (parsed == 8) {
@@ -236,7 +244,7 @@ void bledsignal(const char* direction) {
 void neoledsignal(const char* direction) {
   if (strcmp(direction, "left") == 0) {
     // 왼쪽 15줄을 노란색으로 켜기
-    for (int i = 0; i < 15; i++) {
+    for (int i = 0; i < 17; i++) {
       strip.setPixelColor(i, YELLOW);
       strip.show();  // 한 줄마다 변경된 색상 표시
       delay(6);    // 대기
@@ -250,8 +258,8 @@ void neoledsignal(const char* direction) {
     delay(100);      // 왼쪽 15줄 모두 켜진 후 0.5초 대기
   } else if (strcmp(direction, "right") == 0) {
     // 오른쪽 15줄을 노란색으로 켜기
-    for (int i = 14; i >= 0; i--) {
-      strip.setPixelColor(i + 15, YELLOW);
+    for (int i = 16; i >= 0; i--) {
+      strip.setPixelColor(i + 17, YELLOW);
       strip.show();  // 한 줄마다 변경된 색상 표시
       delay(6);    // 대기
     }
@@ -264,9 +272,9 @@ void neoledsignal(const char* direction) {
     delay(100);      // 모두 끈 후 대기
   } else if (strcmp(direction, "all") == 0) {
     // 왼쪽 15줄과 오른쪽 15줄을 동시에 노란색으로 켜기
-    for (int i = 0; i < 15; i++) {
+    for (int i = 0; i < 17; i++) {
       strip.setPixelColor(i, YELLOW);
-      strip.setPixelColor(i + 15, YELLOW);
+      strip.setPixelColor(i + 17, YELLOW);
     }
     strip.show();  // 변경된 색상 표시
     delay(500);    // 0.5초 대기
